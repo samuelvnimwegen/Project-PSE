abcc6d44d394681162870c67b7e5b8180d712f6e
MoaadK9 <moaadkharicha33@gmail.com>
2023-03-02 15:08:15 +0100

    mogus

---
+++ Trein/cmake-build-debug/CMakeFiles/Trein.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Moaad/Documents/GitHub/Project-PSE/Trein/Station.cpp" "CMakeFiles/Trein.dir/Station.cpp.obj" "gcc" "CMakeFiles/Trein.dir/Station.cpp.obj.d"
+  "C:/Users/Moaad/Documents/GitHub/Project-PSE/Trein/Tram.cpp" "CMakeFiles/Trein.dir/Tram.cpp.obj" "gcc" "CMakeFiles/Trein.dir/Tram.cpp.obj.d"
+  "C:/Users/Moaad/Documents/GitHub/Project-PSE/Trein/TramSysteem.cpp" "CMakeFiles/Trein.dir/TramSysteem.cpp.obj" "gcc" "CMakeFiles/Trein.dir/TramSysteem.cpp.obj.d"
+  "C:/Users/Moaad/Documents/GitHub/Project-PSE/Trein/main.cpp" "CMakeFiles/Trein.dir/main.cpp.obj" "gcc" "CMakeFiles/Trein.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Trein/cmake-build-debug/cmake_install.cmake
+++ Trein/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Samue/Documents/GitHub/Project-PSE/Trein
+# Install script for directory: C:/Users/Moaad/Documents/GitHub/Project-PSE/Trein
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Samue/Documents/GitHub/Project-PSE/Trein/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Moaad/Documents/GitHub/Project-PSE/Trein/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
