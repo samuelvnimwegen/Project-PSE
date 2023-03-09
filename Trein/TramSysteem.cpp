1d4990e3bea2f8fcbd19d285c625f493a273bc4f
samuelvnimwegen <samuelvannimwegen@gmail.com>
2023-03-09 14:48:31 +0100

    ?

--- Trein/TramSysteem.cpp
+++ Trein/TramSysteem.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "TramSysteem.h"
+
+void TramSysteem::openFile(string) {
+    TiXmlDocument doc;
+    if(!doc.LoadFile("eenCD.xml")) {
+        cerr << doc.ErrorDesc() << endl;
+    };
+
+    TiXmlElement* root = doc.FirstChildElement();
+    if (root == nullptr) {
+        cerr << "Failed to load file: No root element." << endl;
+        doc.Clear();
+    }
+
+    for(TiXmlElement* elem = root->FirstChildElement(); elem != nullptr;
+        elem = elem->NextSiblingElement()) {
+        string elemName = elem->Value();
+
+        if(elemName == "ARTIST" or elemName == "TITLE") {
+            cout << elemName << ": "<< elem->GetText() << endl;
+        }
+    }
+}
--- Trein/cmake-build-debug/CMakeFiles/Trein.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Trein.
-# This may be replaced when dependencies are built.
