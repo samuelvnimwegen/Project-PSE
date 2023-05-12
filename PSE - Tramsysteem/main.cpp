4ac7828fea0f0b4df9b4badd57611051b6d01ecf
samuelvnimwegen <samuelvannimwegen@gmail.com>
2023-05-12 15:36:32 +0200

    File met alle verbeterpunten

--- PSE - Tramsysteem/main.cpp
+++ PSE - Tramsysteem/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, const char * argv[]) {
     XMLParser parser = XMLParser();
-    TramSysteem* tramsysteem = parser.readFile("Inconsistentie.xml");
+    TramSysteem* tramsysteem = parser.readFile("happyDay.xml");
     TramSysteemOut* output = new TramSysteemOut("tramregeling.txt", tramsysteem);
     tramsysteem->setOutput(output);
 
