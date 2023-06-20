4981f73f49ba63b0900ffa55d52b63d09504c60d
samuelvnimwegen <samuelvannimwegen@gmail.com>
2023-06-20 12:24:44 +0200

    Afwerkingen

--- PSE - Tramsysteem/main.cpp
+++ PSE - Tramsysteem/main.cpp
@@ -2,17 +2,6 @@
 
 
 int main(int argc, const char * argv[]) {
-    XMLParser parser = XMLParser();
-    TramSysteem* tramsysteem = parser.readFile("happyDay.xml");
-    TramSysteemOut* output = new TramSysteemOut("tramregeling.txt", tramsysteem);
-    tramsysteem->setOutput(output);
-
-    output->advanced_summary();
-    output->complete_summary();
-    tramsysteem->simulate(10);
-
-    output->advanced_summary();
-    output->complete_summary();
     return 0;
 }
 
